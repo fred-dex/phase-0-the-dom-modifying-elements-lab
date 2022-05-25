dc4c0e0672903230cc2a9a7e02d9d7ddfc9f17a5
freddex <fredjuiceman@yahoo.com>
2022-05-24 21:45:03 -0400

    commit tree 839b9e1c6ecd0b7d8b936653942fc38bfd4677a8

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+
+const element = document.createElement("h1#victory");
+
+newHeader.id = "victory"
+newHeader.innerText = "YOUR NAME is the champion!";
+newHeader.className = "victory"
+
+
+
+
